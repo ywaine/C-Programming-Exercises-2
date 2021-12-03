6111b738151a51aaa04c15f877e8b526d661336a
Nikita <93765124+PopadosNikitos@users.noreply.github.com>
2021-12-03 15:53:32 +0300

    Update ex_3.c

--- ex_3.c
+++ ex_3.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+
+int main()
+{
+   int firstint, secondint;
+ printf("first integer = ");
+ scanf("%d", &firstint);
+ printf("second integer = ");
+ scanf("%d", &secondint);
+
+   if(firstint > secondint){
+      printf("%d", firstint);
+    
+   }else if(firstint = secondint){
+         printf("%d", firstint);
+    
+   }else{
+      printf("%d", secondint);
+
+   return 0;
+   }
+}
