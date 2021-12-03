79ba9bd3ed14fb0230eb796c9258b46c3fc364be
Nikita <93765124+PopadosNikitos@users.noreply.github.com>
2021-12-03 15:48:46 +0300

    Update ex_2.c

--- ex_2.c
+++ ex_2.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main()
+{
+	int intg, summ = 0, i;
+	printf("integer = ");
+	scanf("%d", &intg);
+	for (i = 1; i < intg; i++) {
+		summ = summ + i;
+	}
+	printf("%d", summ);
+	return 1;
+}
