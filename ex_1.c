d75132167256c5b30de4234940f30519df3b9488
Nikita <93765124+PopadosNikitos@users.noreply.github.com>
2021-12-03 15:51:43 +0300

    Update ex_1.c

--- ex_1.c
+++ ex_1.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+int main()
+{
+    float height, weight, BMI;
+    printf("Your height in cm is = ");
+    scanf("%f", &height);
+    printf("Your body weight in kg is = ");
+    scanf("%f", &weight);
+    if (height <= 0) {
+        printf("Please enter correct height");
+    }
+    else  if (weight <= 0) {
+        printf("Please enter correct weight");
+    }
+    BMI = weight / ((height * 0.01) * (height * 0.01));
+    printf("%f", BMI);
+}
