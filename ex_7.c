122aa3454c9503dc9cfc82f8b0bf6b7a4e5a7f03
Seong-joo <93765136+Seong-joo@users.noreply.github.com>
2021-12-08 13:21:16 +0100

    Update ex_7.c
    n -> i dont know how to change n into the number that user enter.

--- ex_7.c
+++ ex_7.c
@@ -1,1 +1,23 @@
+/*7. User enters any integer number. System tells whether this number is a prime number or not*/
+#include <stdio.h>
 
+int main() {
+  int n, i, c = 0;
+  printf("Enter any integer number :");
+  scanf_s("%d", &n);
+
+  //logic
+  for (i = 1; i <= n; i++) {
+	  if (n % i == 0) {
+		 c++;
+	  }
+  }
+
+  if (c == 2) {
+  printf("n is a Prime number");
+  }
+  else {
+  printf("n is not a Prime number");
+  }
+  return 0;
+}
