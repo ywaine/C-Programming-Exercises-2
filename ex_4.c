f56ad2ff226767604858d42d112bd282e57ec10b
Seong-joo <93765136+Seong-joo@users.noreply.github.com>
2021-12-09 16:59:36 +0100

    Update ex_4.c

--- ex_4.c
+++ ex_4.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
 
+float main()
+
+{
+
+	float a, b, c, sum;
+
+	float d;
+	
+	printf("Please enter 3 real numbers :");
+
+	scanf_s("%f%f%f", &a, &b, &c);
+
+	sum = a + b + c;
+
+	d = (a + b + c) / 3;
+
+	printf("\nThe average is %f", d);
+
+	return 0;
+
+}
