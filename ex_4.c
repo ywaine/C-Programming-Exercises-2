322f35b035bdc4b887304eca1d17d30545480641
Antony rubombora <48180231+tonyrubombora@users.noreply.github.com>
2021-12-10 11:53:55 +0300

    Update ex_4.c

--- ex_4.c
+++ ex_4.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+
+void main()
+
+{
+
+	float a, b, c, d;
+
+	printf("Please enter 3 real numbers :");
+
+	scanf("%f%f%f", &a, &b, &c);
+	
+	float averageCalculate(float a, float b, float c);
+	
+  d = averageCalculate(a,b,c);
+	
+	printf("\nThe average is %f", d);
+
+}
+
+float averageCalculate(float a, float b, float c){
+    float sum = a + b + c;
+
+	float d = (a + b + c) / 3;
+	
+	return d;
+}
