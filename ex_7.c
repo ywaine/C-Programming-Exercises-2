566583a562f243f458108fb138c07d152d2e439f
Antony rubombora <48180231+tonyrubombora@users.noreply.github.com>
2021-12-10 13:50:18 +0300

    tony edited ex_7

--- ex_7.c
+++ ex_7.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+int main() {
+	int n, output;
+	printf("Enter any integer number : ");
+	int isInt = scanf("%i", &n);
+	if(isInt==0){
+	    printf("Please enter a valid integer number: ");
+	    return 0;
+	}
+    int primeCheck(int n);
+    output = primeCheck(n);
 
+		if(output == 1){
+			printf("%i is a prime number.", n);
+		}else if(output == 0){
+			printf("%i is not a prime number.", n);
+		}
+}
+
+int primeCheck(int n){
+    
+	for (int i = 2; i <= n / 2; i++) {
+		// condition for prime
+		if (n % i == 0) {
+			return 1;
+			break;
+		}
+	}
+
+	
+}
