7ca0e9ef23e5f70902b01babb70b32c3144b07c3
Seong-joo <93765136+Seong-joo@users.noreply.github.com>
2021-12-13 23:35:10 +0100

    Update ex_2.c

--- ex_2.c
+++ ex_2.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	int n;
 
+	printf("Enter any integer = ");
+	scanf_s("%d", &n);
+	int sum = (n * (n + 1)) / 2;
+		printf("The summation from 1 to %d is", n);
+		printf("% d", sum);
+}
