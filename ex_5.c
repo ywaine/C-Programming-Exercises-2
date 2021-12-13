3534f44d15b82ad47908239f3cf9035288a841cb
Nikita <93765124+PopadosNikitos@users.noreply.github.com>
2021-12-13 18:00:39 +0200

    Update ex_5.c

--- ex_5.c
+++ ex_5.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+int integer;
+printf("Enter your integer: ");
+scanf("%d",&integer);
+int seven = 7;
+int three = 3;
+int main ()
+{
+for(int i=0;i <integer; i++)
+{
+    if(i%seven==0 && i%three==0)
+    {
+    printf("%d/n", i);
+    }
+}
+return 0;
+}
