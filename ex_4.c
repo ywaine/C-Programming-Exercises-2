51ab68f1e77cbbcebc48383da922b0f62da0131d
Antony rubombora <48180231+tonyrubombora@users.noreply.github.com>
2021-12-14 20:52:09 +0300

    Added data validation

--- ex_4.c
+++ ex_4.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
 
-void main()
+int main()
 
 {
 
 	float a, b, c, d;
 
-	printf("Please enter 3 real numbers :");
+	printf("Please enter 3 real numbers: ");
 
-	scanf("%f%f%f", &a, &b, &c);
+if(scanf("%f", &a)==0){
+	    printf("Please input a valid number");
+	    return 1;
+	}
+	
+	if(scanf("%f", &b)==0){
+	    printf("Please input a valid number");
+	    return 1;
+	}
+	
+	if(scanf("%f", &c)==0){
+	    printf("Please input a valid number");
+	    return 1;
+	}
 	
 	float averageCalculate(float a, float b, float c);
 	
@@ -17,13 +30,11 @@ void main()
 	
 	printf("\nThe average is %f", d);
 
+	return 0;
 }
 
 float averageCalculate(float a, float b, float c){
     float sum = a + b + c;
 
 	float d = (a + b + c) / 3;
-	
-	return d;
-
 }
