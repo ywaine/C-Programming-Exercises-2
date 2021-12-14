864221ce5b37b3f6b7e2a81ccfb31f0d83bd117c
Nikita <93765124+PopadosNikitos@users.noreply.github.com>
2021-12-14 23:45:35 +0300

    Update ex_5.c

--- ex_5.c
+++ ex_5.c
@@ -1,16 +1,19 @@
+/*Write a program that prints all common multiples of 3 and 7 between 1 and the
+integer entered by the user. Use global variables to store 3 and 7. 
+*/
 #include <stdio.h>
-int integer;
-printf("Enter your integer: ");
-scanf("%d",&integer);
 int seven = 7;
 int three = 3;
 int main ()
 {
-for(int i=0;i <integer; i++)
+int integer;
+printf("Enter your integer: ");
+scanf("%d", &integer);
 {
+for(int i=1;i <integer; i++)
     if(i%seven==0 && i%three==0)
     {
-    printf("%d/n", i);
+    printf("%d\n", i);
     }
 }
 return 0;
