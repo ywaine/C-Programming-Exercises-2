5b26ffd027e5017ede8465155dc69334de87b37d
Antony rubombora <48180231+tonyrubombora@users.noreply.github.com>
2021-12-14 20:55:24 +0300

    Added data validation

--- ex_2.c
+++ ex_2.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-void main()
+int main()
 {
 	int n;
 
 	printf("Enter any integer = ");
-	scanf_s("%d", &n);
+	if(scanf("%d", &n)==0){
+	    printf("Please input a valid integer");
+	    return 1;
+	}
 	int sum = (n * (n + 1)) / 2;
 		printf("The summation from 1 to %d is", n);
 		printf("% d", sum);
+		return 0;
 }
