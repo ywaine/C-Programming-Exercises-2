08edfd7d0ab36777acd113fb07d61f57faf9144d
Antony rubombora <48180231+tonyrubombora@users.noreply.github.com>
2021-12-14 21:36:22 +0300

    Variables must be Global

--- ex_5.c
+++ ex_5.c
@@ -1,14 +1,14 @@
 /*5. Print common multiples of 3 and 7 btw 1 to n which is entered by user */
 #include <stdio.h>
-int main()
-{
-	int i;
+
 	int a = 3;
 	int b = 7;
-	int n;
-	int c = 0;
+	
+int main()
+{
+	int i, n, c = 0;
 	printf("Enter any integer number: ");
-	scanf_s("%d", &n);
+	scanf("%d", &n);
 	printf("The common multiples between 1 and %i are: \n", n);
 	for (i = 1; i < n; i++) {
 		//conditional statement: if i/21 has no reminder
@@ -20,7 +20,8 @@ int main()
 	if (c==0)
 	{
 		printf("There is no common multiple between 1 and %d \n", n);
+	    return 1;
+	    
 	}
 	return 0;
 }
-
