f0cac9e2bdb7564718ae4ad0e5ed6b9fbfe32c6c
Antony rubombora <48180231+tonyrubombora@users.noreply.github.com>
2021-12-14 19:57:04 +0300

    Tony update ex_1.c with rerunability

--- ex_1.c
+++ ex_1.c
@@ -3,15 +3,20 @@ int main()
 {
     float height, weight, BMI;
     printf("Your height in cm is = ");
-    scanf_s("%f", &height);
-    printf("Your body weight in kg is = ");
-    scanf_s("%f", &weight);
+    scanf("%f", &height);
     if (height <= 0) {
         printf("Please enter correct height");
+         // Rerun method if input is invalid
+        main();
     }
-    else  if (weight <= 0) {
+    printf("Your body weight in kg is = ");
+    scanf("%f", &weight);
+    if (weight <= 0) {
         printf("Please enter correct weight");
+        // Rerun method if input is invalid
+        main();
     }
-    BMI = weight / ((height * 0.01) * (height * 0.01));
+    BMI = (weight / ((height * 0.01) * (height * 0.01)));
     printf("%f", BMI);
+    return 1;
 }
