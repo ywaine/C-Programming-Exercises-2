52be32717b41f447f3f77807fa1e5fbd7831f89c
Antony rubombora <48180231+tonyrubombora@users.noreply.github.com>
2021-12-14 20:40:26 +0300

    Added data validation

--- ex_3.c
+++ ex_3.c
@@ -4,20 +4,29 @@
 int main()
 {
    int firstint, secondint;
- printf("first integer = ");
- scanf("%d", &firstint);
- printf("second integer = ");
- scanf("%d", &secondint);
-
+ printf("First integer = ");
+  if(scanf("%d", &firstint)==0){
+       printf("\nPlease enter a valid integer \n");
+       return 1;
+       
+  } else{
+ printf("Second integer = ");
+ if(scanf("%d", &secondint)==0){
+       printf("\nPlease enter a valid integer \n");
+       return 1;
+  }
+}
    if(firstint > secondint){
-      printf("%d", firstint);
-    
-   }else if(firstint = secondint){
-         printf("%d", firstint);
-    
-   }else{
-      printf("%d", secondint);
-
+      printf("%d is larger", firstint);
+    return 0;
+   }else if(firstint == secondint){
+         printf("%d (is the same)", firstint);
+    return 0;
+   }else if(firstint>secondint){
+      printf("%d is larger", secondint);
    return 0;
+   } else{
+       printf("Please enter a valid integer");
+      return 1;
    }
 }
